75fe3daaaf249e40dffd1c211fe649a42378cbae
Bella Amandine <bellamandy45@gmail.com>
2021-04-30 16:18:13 +0200

    Test

---
+++ main.js
@@ -0,0 +1,35 @@
+let selectedFile;
+console.log(window.XLSX);
+document.getElementById('input').addEventListener("change", (event) => {
+    selectedFile = event.target.files[0];
+})
+
+let data = [{
+    "name": "jayanth",
+    "data": "scd",
+    "abc": "sdef"
+}]
+
+
+document.getElementById('button').addEventListener("click", () => {
+    XLSX.utils.json_to_sheet(data, 'out.xlsx');
+    if (selectedFile) {
+        let fileReader = new FileReader();
+        fileReader.readAsBinaryString(selectedFile);
+        fileReader.onload = (event) => {
+            //get binary result
+            let data = event.target.result;
+
+            let workbook = XLSX.read(data, {
+                type: "binary"
+            });
+            console.log(workbook);
+            workbook.SheetNames.forEach(sheet => {
+                let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
+                console.log(rowObject);
+                console.log(JSON.stringify(rowObject, undefined, 4))
+                document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject, undefined, 4)
+            });
+        }
+    }
+});
